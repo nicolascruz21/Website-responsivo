e59de29db7983d39a896f397981734971b624393
Nicolas Farias <105246373+nicolascruz21@users.noreply.github.com>
2024-03-04 15:04:43 -0300

    Att animacao

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 window.sr = ScrollReveal ({ reset: true});
 
 sr.reveal('.apresentacao', {duration: 2000})
-sr.reveal('.servicos__section', {duration: 2000})
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('nav a, .lista-menu a', '.servicos__cards a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const targetPosition = targetElement.offsetTop;
+
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
